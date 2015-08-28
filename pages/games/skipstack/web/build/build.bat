5abee9cae47329b98ab9bb17fb93487f3397f6f5
Mourtz <alex.mourtziapis@gmail.com>
2015-08-28 16:45:00 +0300

    Update SkipStack to the latest build .

--- pages/games/skipstack/web/build/build.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-call npm install uglify-js -g
-call uglifyjs ../src/Assets/Shaders.js ../src/Assets/Tunes.js ../src/Actor.js ../src/Cell.js ../src/Color.js ../src/Enemy.js ../src/Grid.js ../src/index.js ../src/Math.js ../src/Swipe.js ../src/uiBuilder.js ../src/uiDivider.js  ../src/uiGeneric.js ../src/uiIndex.js ../src/uiSelector.js -o skipstack.min.js --source-map skipstack.min.js.map -c -m
-PAUSE
